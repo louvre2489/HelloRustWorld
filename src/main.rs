4c4b22e3b8bed3491a666efdbb67954ccfdd9692
louvre2489 <louvre2489@gmail.com>
2020-12-20 06:56:46 +0900

    引数で指定したファイルから内容を読み出しする

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use clap::Clap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Nori",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    /// Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+
+    /// Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
+fn main() {
+    let opts = Opts::parse();
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            println!("{}", line)
+        }
+    } else {
+        println!("No file is specified");
+    }
+}
